ea8df7318473ed4032e17d03fa77b7a95d82468f
Andrew Bullock <trullock@gmail.com>
2019-04-29 07:59:18 +0100

    made local iis work, moved assets, fixed service worker

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,17 @@
 function refresh(){
 	return caches.open('offline-v1').then(function(cache){
 		return Promise.all([
+			cache.add('/'),
             cache.add('/core-competencies.js'),
             cache.add('/engineering-competencies.js'),
             cache.add('/role-mappings.js'),
-            cache.add('/styles/index.css'),
-            cache.add('/scripts/handlebars-v4.1.2.js'),
-            cache.add('/scripts/index.js'),
-            cache.add('/images/favicon.ico'),
-            cache.add('/images/logo.png'),
+            cache.add('/index.css'),
+            cache.add('/handlebars-v4.1.2.js'),
+			cache.add('/index.js'),
+			cache.add('/logo.png'),
+            cache.add('/favicon.ico'),
+            cache.add('/favicon-16x16.png'),
+            cache.add('/favicon-32x32.png'),
             cache.add('https://fonts1.unidays.world/unidays/v1/all-book.woff2'),
             cache.add('https://fonts1.unidays.world/unidays/v1/all-demi.woff2'),
             cache.add('https://fonts1.unidays.world/unidays/v1/all-heavy.woff2')
@@ -27,13 +30,16 @@ self.addEventListener('activate', function(event) {
 	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-	
-	var result = fetch(event.request).catch(function(error) {
-		return caches.match(event.request);
-	});
-	
-	event.respondWith(result);
+self.addEventListener('fetch', (event) => {
+	event.respondWith(async function() {
+		try {
+			return await fetch(event.request);
+		} catch (err) {
+			if(event.request.mode === 'navigate')
+				return caches.match('/');
+			return caches.match(event.request);
+		}
+	}());
 });
 
 self.addEventListener('message', function(msg){
