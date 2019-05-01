f16318e4e03529e53af892aaa905eb3fdd15a95a
Andrew Bullock <trullock@gmail.com>
2019-05-01 09:24:41 +0100

    tidied values contnet

--- core-values.js
+++ core-values.js
@@ -3,76 +3,79 @@
     path: "core-values",
     topics: [
         {
-            title: "Everyone is important",
+            title: "🙋‍♀️ Everyone is important",
             path: "everyone-is-important",
             summary: "",
             levels: [
                 {
-                    title: "👩‍🎓 n00b",
-                    path: "n00b",
-                    summary: "Commercially Aware",
+                    title: "Participant",
+                    path: "participant",
+                    summary: "",
                     criteria: [
-                          ""
+                          "Identifies, influences, engages, and communicates well with internal and external stakeholders to make better decisions faster and with greater collaboration.",
+                          "Coaches, mentors, develops, and influences others in order to help achieve business objectives."
                     ]
                 }
             ]
         },
         {
-            title: "No BS, no jerks",
+            title: "🙊 No BS, no jerks",
             path: "no-bs-no-jerks",
             summary: "",
             levels: [
                 {
-                    title: "👩‍🎓 n00b",
-                    path: "n00b",
-                    summary: "Commercially Aware",
+                    title: "Participant",
+                    path: "participant",
+                    summary: "",
                     criteria: [
-                          ""
+                          "Effectively and appropriately communicates and adapts communication style in various situations to achieve a positive impact."
                     ]
                 }
             ]
         },
         {
-            title: "Be clear on your purpose",
+            title: "🎯 Be clear on your purpose",
             path: "be-clear-on-your-purpose",
             summary: "",
             levels: [
                 {
-                    title: "👩‍🎓 n00b",
-                    path: "n00b",
-                    summary: "Commercially Aware",
+                    title: "Participant",
+                    path: "participant",
+                    summary: "",
                     criteria: [
-                          ""
+                          "Focusses time and expertise to have the greatest impact on members, partners, and critical business needs."
                     ]
                 }
             ]
         },
         {
-            title: "Be a builder",
-            path: "be a builder",
+            title: "👷 Be a builder",
+            path: "be-a-builder",
             summary: "",
             levels: [
                 {
-                    title: "👩‍🎓 n00b",
-                    path: "n00b",
-                    summary: "Commercially Aware",
+                    title: "Participant",
+                    path: "participant",
+                    summary: "",
                     criteria: [
-                          ""
+                          "Solves business problems with appropriate business solutions.",
+                          "Involves different perspectives and expertise at all levels and across departments to achieve the best results."
                     ]
                 }
             ]
         },
         {
-            title: "Jump into the unknown",
+            title: "❓ Jump into the unknown",
             path: "jump-into-the-unknown",
             summary: "",
             levels: [
                 {
-                    title: "👩‍🎓 n00b",
-                    path: "n00b",
-                    summary: "Commercially Aware",
+                    title: "Participant",
+                    path: "participant",
+                    summary: "",
                     criteria: [
-                          ""
+                          "Responds to change with a willingness to learn, so they can help achieve their individual and business objectives.",
+                          "Produces new product ideas or creative solutions that have never been imagined before."
                     ]
                 }
             ]
