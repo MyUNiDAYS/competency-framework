2127f72747f568385318a211072cbe191befafcd
Andrew Bullock <trullock@gmail.com>
2019-05-08 13:18:41 +0100

    made review only contain required level and below

--- index.js
+++ index.js
@@ -107,6 +107,48 @@ window.addEventListener('load', function(){
         return competencies[0].level.title;
     });
 
+    Handlebars.registerHelper('ifLevelEqual', function (topic, level, expectedLevel, options) {
+        var expectedIndex = topic.levels.indexOf(expectedLevel);
+        var index = topic.levels.indexOf(level);
+
+        if(expectedIndex === -1 || index === -1)
+            return;
+
+        if(index === expectedIndex)
+            return options.fn(this);
+        
+        return;
+    });
+
+    
+    Handlebars.registerHelper('ifLevelLowerOrEqual', function (topic, level, expectedLevel, options) {
+        var expectedIndex = topic.levels.indexOf(expectedLevel);
+        var index = topic.levels.indexOf(level);
+
+        if(expectedIndex === -1 || index === -1)
+            return;
+
+        if(index <= expectedIndex)
+            return options.fn(this);
+        
+        return;
+    });
+
+
+    Handlebars.registerHelper('levelRequirementClass', function (topic, level, expectedLevel) {
+        var expectedIndex = topic.levels.indexOf(expectedLevel);
+        var index = topic.levels.indexOf(level);
+
+        if(expectedIndex === -1 || index === -1)
+            return '';
+
+        if(index < expectedIndex)
+            return 'level-lower'
+        if(index == expectedIndex)
+            return 'level-equal';
+        return 'level-above'
+    });
+
 
     // Initialise content
     document.querySelector('.container > nav').innerHTML += templates['nav-competencies'](window.competencies) + templates['nav-roles'](window.roles);
