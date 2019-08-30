db49406c9ef7d4a0dfa2520bc3a18d8e391bce6a
Andrew Bullock <trullock@gmail.com>
2019-08-30 09:35:16 +0100

    got url compression working

---
+++ src/js/compressor.js
@@ -0,0 +1,23 @@
+window.compressor = {
+
+    compress: function(data) {
+        var query = Object.keys(data).reduce((str, key) => str + '&' + key + '=' + data[key], '').substr(1);
+        return btoa(new LZ77().compress(query)).replace(/\+/g, '-').replace(/\//g, '_');
+    },
+
+    decompress: function(str){
+
+        var b64 = str.replace(/-/g, '+').replace(/_/g, '/');
+        var compressed = atob(b64);
+
+        var query = new LZ77().decompress(compressed);
+
+        var kvps = query.split('&');
+
+        return kvps.reduce((agg, x) => {
+            var parts = x.split('=');
+            agg[parts[0]] = parts[1];
+            return agg;
+        }, {});
+    }
+};
